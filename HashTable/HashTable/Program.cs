1ce77d071934f9d3fb8ef4d7a78cdbb591f46bd8
rohantare03 <rohatare123@gmail.com>
2022-05-26 11:13:25 +0530

    [Rohan] Add : Finding Frequency of a large phrase

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -8,7 +8,7 @@ namespace HashTable
             //uc1 : find frequency of word in sentence
             Console.WriteLine("Welcome to Hashtable");
             LinkedHashMap<string, int> linkedHashMap = new LinkedHashMap<string, int>(5);
-            string sentence = "to be or not to be";
+            string sentence = " paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
             string[] words = sentence.ToLower().Split(" ");
             foreach (string word in words)
             {
@@ -21,14 +21,34 @@ namespace HashTable
                     value += 1;
                 linkedHashMap.Add(word, value);
             }
-            int FrequencyTo = linkedHashMap.Get("to");
-            int FrequencyBe = linkedHashMap.Get("be");
-            int FrequencyOr = linkedHashMap.Get("or");
+            int FrequencyParanoids = linkedHashMap.Get("paranoids");
+            int FrequencyAre = linkedHashMap.Get("are");
             int FrequencyNot = linkedHashMap.Get("not");
-            Console.WriteLine("The Frequency of word to is :" + FrequencyTo);
-            Console.WriteLine("The Frequency of word be is :" + FrequencyBe);
-            Console.WriteLine("The Frequency of word or is :" + FrequencyOr);
+            int FrequencyParanoid = linkedHashMap.Get("paranoid");
+            int FrequencyBecause = linkedHashMap.Get("because");
+            int FrequencyThey = linkedHashMap.Get("they");
+            int FrequencyBut = linkedHashMap.Get("but");
+            int FrequencyKeep = linkedHashMap.Get("keep");
+            int FrequencyPutting = linkedHashMap.Get("putting");
+            int FrequencyThemselves = linkedHashMap.Get("themselves");
+            int FrequencyDeliberately = linkedHashMap.Get("deliberately");
+            int FrequencyInto = linkedHashMap.Get("into");
+            int FrequencyAvoidable = linkedHashMap.Get("avoidable");
+            int FrequencySituations = linkedHashMap.Get("situations");
+            Console.WriteLine("The Frequency of word Paranoids is :" + FrequencyParanoids);
+            Console.WriteLine("The Frequency of word are is :" + FrequencyAre);         
             Console.WriteLine("The Frequency of word not is :" + FrequencyNot);
+            Console.WriteLine("The Frequency of word paranoid is :" + FrequencyParanoid);
+            Console.WriteLine("The Frequency of word because is :" + FrequencyBecause);
+            Console.WriteLine("The Frequency of word they is :" + FrequencyThey);
+            Console.WriteLine("The Frequency of word but is :" + FrequencyBut);
+            Console.WriteLine("The Frequency of word keep is :" + FrequencyKeep);
+            Console.WriteLine("The Frequency of word putting is :" + FrequencyPutting);
+            Console.WriteLine("The Frequency of word themselves is :" + FrequencyThemselves);
+            Console.WriteLine("The Frequency of word deliberately is :" + FrequencyDeliberately);
+            Console.WriteLine("The Frequency of word into is :" + FrequencyInto);
+            Console.WriteLine("The Frequency of word avoidable is :" + FrequencyAvoidable);
+            Console.WriteLine("The Frequency of word situations is :" + FrequencySituations);
         }
     }
 }
